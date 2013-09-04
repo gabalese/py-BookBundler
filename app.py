cafe1365ab2d086b2c5d74a55c1a4b89c0ec884e
gabalese <gabriele@alese.it>
2013-09-04 14:09:20 +0200

    Working beta state

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import subprocess
-from flask import Flask, request
+from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
 
 from PIL import Image
@@ -36,31 +36,80 @@ def upload_file():
     if request.method == 'POST':
         sent_file = request.files['file']
         if sent_file and allowed_file(sent_file.filename):
-            filename = secure_filename(sent_file.filename)
 
+            # escape malicious filename
+            filename = secure_filename(os.tempnam()+sent_file.filename)
+
+            # save image in upload folder
             sent_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
+            # use PIL to fix orientation EXIF data for iPhone
+            # TODO: is this valid for Android too?
             fix_orientation(os.path.join(app.config['UPLOAD_FOLDER'], filename), save_over=True)
 
+            # open image and convert to BW
             img = Image.open(os.path.join(app.config['UPLOAD_FOLDER'], filename)).convert('LA')
 
+            # save BW'd image on disk
             img_name = os.tempnam("uploads/", "img_")+".png"
             img.save(img_name)
 
+            # temporary filename
             temp = os.tempnam("uploads/", "tess_")
 
+            # prepare tesseract shell spawn
             command = ["tesseract", img_name, temp, "-l ita"]
 
-            proc = subprocess.Popen(command)
+            try:
+                # spawn process, wait and intercept any non-zero exit status
+                # mute stdin and stderr
+                subprocess.check_call(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            except subprocess.CalledProcessError:
+                return "Command unsuccessfull!"
 
-            proc.wait()
+            # temporary file context
+            # replace with call to mongo
+            with open("isola_target.txt") as f:
+                source = f.readlines()
 
-            if matches("isola_target.txt", temp+".txt"):
-                return "OOOOOK"
+            # this one will last
+            with open(temp+".txt") as g:
+                destination = g.readlines()
+
+            if matches(source, destination):  # the fixed file should be replaced with an array from dict
+                # main OK response call
+                return """
+                        <!doctype html>
+                        <html style="height: 100%">
+                        <head>
+                        <title>Success!</title>
+                        </head>
+                        <body style="height: 100%">
+                        <p style="background-color: green; min-height: 100%;">&nbsp;</p>
+                        </body>
+                        </html>
+                        """
             else:
-                return "NOOOOOO!!!"
+                # main KO response call
+                # render a fail template?
+                return """
+                        <!doctype html>
+                        <html style="height: 100%">
+                        <head>
+                        <title>Fail!</title>
+                        </head>
+                        <body style="height: 100%">
+                        <p style="background-color: red; min-height: 100%;">&nbsp;</p>
+                        </body>
+                        </html>
+                        """
+        else:
+        # if not allowed_file ...
+            return "NOPE."
+            # I should throw a proper HTTP Status instead...
 
-    return '''
+    if request.method == 'GET':
+        return '''
                 <!doctype html>
                 <title>Upload new File</title>
                 <h1>Upload new File</h1>
--- matching.py
+++ matching.py
@@ -1,10 +1,15 @@
-# logica di base del matching
-def matches(file1, file2):
-    lista1 = open(file1, "r").readlines()
-    lista2 = open(file2, "r").readlines()
-
-    for i in lista1:
-        for v in lista2:
+def matches(source, destination):
+    """
+    Basic mathing algorithm. Double iteration over src & dst, if the tuple of len matches for a line,
+    find a match of string contents. One match is considered exhaustive, for now.
+    :param source: list of input text
+    :type source: list
+    :param destination: list of reference text
+    :type destination: list
+    :rtype: bool
+    """
+    for i in source:
+        for v in destination:
             if map(len, i.split(" ")) == map(len, v.split(" ")):
                 if i == v:
                     return True
